aacb78ceb30ed360c334425336cd4f462c040fe5
Linda <romsmomm@yahoo.com>
2021-05-03 16:11:58 -0700

    making changes

--- migrations/V2.2__InsertRowsInILikeDags.sql
+++ migrations/V2.2__InsertRowsInILikeDags.sql
@@ -2,5 +2,6 @@ INSERT dbo.ILikeDags (Declaration)
 VALUES 
     ('I also like dags'),
     ('I think this is a fight club reference'),
+    ('I think this is a fight club reference.  For me'),
     ('You can step in the same river twice');
 
--- migrations/V3__YOURENOTMYREALMOM.sql
+++ migrations/V3__YOURENOTMYREALMOM.sql
@@ -1,4 +1,4 @@
-CREATE TABLE dbo.YoureNotMyRealMom(    
+CREATE TABLE dbo.YoureNotMyRealMom (    
      YoureNotMyRealMomId INT IDENTITY NOT NULL,    
      Declaration nvarchar(1000) NULL,    
      CONSTRAINT PK_YoureNotMyRealMom PRIMARY KEY CLUSTERED (YoureNotMyRealMom)
