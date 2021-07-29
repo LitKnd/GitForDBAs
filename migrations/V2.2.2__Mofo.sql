97e3efce8508a0617b2c4d18ee2df0f6af644156
David Ferris <David.Ferris@bozzuto.com>
2021-07-29 13:36:53 -0500

    adding the 2 mofo files

---
+++ migrations/V2.2.2__Mofo.sql
@@ -0,0 +1,5 @@
+create table dbo.FoolishMonkey (
+    id bigint identity(1,1) primary KEY,
+    firstName varchar(255) null,
+    lastName varchar(255) null
+);
---
+++ migrations/V2.2.2__Mofo2.sql
@@ -0,0 +1,5 @@
+create table dbo.RockOn (
+    ID bigint identity(1,1) not null primary key,
+    firstName varchar(255) NULL,
+    lastName varchar(255) null
+)
