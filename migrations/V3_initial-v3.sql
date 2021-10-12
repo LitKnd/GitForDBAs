087c335724e21d7c464e00ca045108da714cf721
duch5558 <christian.dupont.contractuel@msss.gouv.qc.ca>
2021-10-12 08:39:20 -0400

    My second Commit

--- migrations/V3_initial-v3.sql
+++ migrations/V3_initial-v3.sql
@@ -3,3 +3,8 @@ CREATE TABLE dbo.YoureNotMyRealMom(
      Declaration nvarchar(1000) NULL,    
      CONSTRAINT PK_YoureNotMyRealMom PRIMARY KEY CLUSTERED (YoureNotMyRealMom)
 );
+CREATE TABLE dbo.YoureNotMyRealMom(    
+     YoureNotMyRealMomId INT IDENTITY NOT NULL,    
+     Declaration nvarchar(1000) NULL,    
+     CONSTRAINT PK_YoureNotMyRealMom PRIMARY KEY CLUSTERED (YoureNotMyRealMom)
+);
