36e0da26e44901f64ed1841c5cf45a1ecd9a23c3
NK-GitUser <Naeem.Khan@T-Mobile.com>
2021-11-22 21:22:38 -0500

    just a test1

---
+++ migrations/V3__nk.sql
@@ -0,0 +1,5 @@
+CREATE TABLE dbo.YoureNotMyRealMom(    
+     YoureNotMyRealMomId INT IDENTITY NOT NULL,    
+     Declaration nvarchar(1000) NULL,    
+     CONSTRAINT PK_YoureNotMyRealMom PRIMARY KEY CLUSTERED (YoureNotMyRealMom)
+);
