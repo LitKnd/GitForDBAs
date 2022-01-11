7724dccd24aea4529bb874609a79a4546168cfb1
ericjiang20 <64163523+ericjiang20@users.noreply.github.com>
2022-01-11 16:26:25 -0500

    am I ready to commit?

---
+++ migrations/V3.1__ericscript001.sql
@@ -0,0 +1,5 @@
+CREATE TABLE dbo.Eric_test(    
+     YoureNotMyRealMomId INT IDENTITY NOT NULL,    
+     Declaration nvarchar(1000) NULL,    
+     CONSTRAINT PK_YoureNotMyRealMom1 PRIMARY KEY CLUSTERED (YoureNotMyRealMom)
+);
