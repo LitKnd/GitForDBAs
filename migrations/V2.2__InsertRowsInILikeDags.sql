1a103bf833a033551299d4545231b6de7ffb3d9d
Eric Rasinski <eric.rasinski@gmail.com>
2022-09-23 19:54:16 -0500

    new branch

--- migrations/V2.2__InsertRowsInILikeDags.sql
+++ migrations/V2.2__InsertRowsInILikeDags.sql
@@ -2,5 +2,6 @@ INSERT dbo.ILikeDags (Declaration)
 VALUES 
     ('I also like dags'),
     ('I think this is a fight club reference'),
-    ('You can step in the same river twice');
+    ('You can step in the same river twice'),
+    ('Another row');
 
