303581918c8fcc6dd954273ad085313a27d77b51
Eric Thomassin <DRL9169@mvtdesjardins.com>
2023-02-15 16:20:11 -0500

    mon premier commit

--- migrations/V2__YOLO.sql
+++ migrations/V2__YOLO.sql
@@ -1,6 +1,6 @@
 INSERT HelloWorld (Declaration)
 VALUES 
     ('They''re all good dogs brent'),
-    ('I am a bear'),
+    ('I am a beareeeeeeeeeee'),
     ('You cannot step in the same river twice');
 
