532ebd6cc0915473cb1a29e9ed2bb4f946c4dda9
Paritosh Tripathi <tparitos@amazon.com>
2023-10-23 23:05:50 -0400

    ready to commot

---
+++ migrations/V4__YOURENOTMYREALMOM.sql
@@ -0,0 +1,5 @@
+CREATE TABLE dbo.YoureNotMyRealMom1(    
+     YoureNotMyRealMomId INT IDENTITY NOT NULL,    
+     Declaration nvarchar(1000) NULL,    
+     CONSTRAINT PK_YoureNotMyRealMom1 PRIMARY KEY CLUSTERED (YoureNotMyRealMom1)
+);
